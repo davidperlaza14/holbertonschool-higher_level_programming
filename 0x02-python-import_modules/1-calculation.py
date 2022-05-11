b82ffebf9754bec052b539e43b318698a37f2db6
davidperlaza14 <davidperlaza14@gmail.com>
2022-05-11 15:03:15 -0500

    1-calculation.py 2-args

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    from calculator_1 import add, sub, mul, div
 
-    argc = len(argv)
-
-    if argc == 1:
-        word = "arguments."
-    elif argc == 2:
-        word = "argument:"
-    else:
-        word = "arguments:"
-
-    print("{:d} {:s}".format(argc - 1, word))
-
-    for num, value in enumerate(argv):
-        if num > 0:
-            print("{:d}: {:s}".format(num, value))
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
