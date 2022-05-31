4a47737959462394e35171375d814a9a3fc336ef
davidperlaza14 <davidperlaza14@gmail.com>
2022-05-31 16:26:10 -0500

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Documentation for square class"""
+
+
+class Square():
+    """Square class"""
+
+    def __init__(self, size):
+        """Initialize the square object
+        Args:
+            size (int): the size of the square"""
+
+        self.__size = size
