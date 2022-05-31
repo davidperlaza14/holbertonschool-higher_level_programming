f63627138e948ef061247913ccc30913871b253a
davidperlaza14 <davidperlaza14@gmail.com>
2022-05-31 16:46:05 -0500

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,17 +7,17 @@ class Square():
 
     def __init__(self, size=0):
         """Sets the size of the square on instantation
-        Throws an error if the size called with it is not an integer
+           Throws an error if the size called with it is not an integer
         Args:
-        size (int, optional): The size of the square object
+            size (int, optional): The size of the square object
         Raises:
-        TypeError: When the value passed in is not an integer
-        ValueError:When the vale passed in is less than 0
+            TypeError: When the value passed in is not an integer
+            ValueError: When the value passed in is less than 0
         """
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
