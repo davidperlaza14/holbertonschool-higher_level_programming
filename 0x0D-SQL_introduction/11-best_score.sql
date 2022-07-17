10d7780f77ea56d9dc62e7261ed77202f9165cc6
davidperlaza14 <davidperlaza14@gmail.com>
2022-07-17 13:53:02 -0500

    11-best_score.sql

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- display both the score and the name (in this order)
+SELECT score, name FROM second_table WHERE  score >=10  ORDER BY score DESC;
