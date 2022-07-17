4828c68417bdd5d910ce40f4c477622aa59f39db
davidperlaza14 <davidperlaza14@gmail.com>
2022-07-17 12:31:06 -0500

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,6 +3,6 @@
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 -- add multiples rows.
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'ALEX', 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'BOb', 14);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
