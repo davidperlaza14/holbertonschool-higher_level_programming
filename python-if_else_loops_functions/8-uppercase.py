2e97f4b1c2b13e96ae1336f031bc93b091133577
davidperlaza14 <davidperlaza14@gmail.com>
2022-09-20 19:13:01 -0500

    8. To uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in str:
         convr = ord(i)
         if convr in range(97, 123):
-            convr =  convr - 32
-            print('{:c}'.format(chr(str)), end='')
+            convr = convr - 32
+        print("{:c}".format(convr), end="")
     print()
