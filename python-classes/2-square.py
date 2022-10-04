b1488a4146af060d8e0c242ddaf1208e6d40b293
davidperlaza14 <davidperlaza14@gmail.com>
2022-10-03 21:10:39 -0500

    2. Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,13 +6,13 @@ size must be an int and greater than zero
 
 
 class Square:
-    """Define a private instance att"""
-    def __init__(self, size = 0):
+    "Define a private instance att"
+    def __init__(self, size=0):
 
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        
+
         if size < 0:
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
 
         self.__size = size
