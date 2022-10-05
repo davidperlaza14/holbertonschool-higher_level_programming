83e01e1449b12b15ba6d4a3c1b3cfeec6a1a74bb
davidperlaza14 <davidperlaza14@gmail.com>
2022-10-04 20:51:08 -0500

    4. Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 class Square:
+    """ An class called Square """
     def __init__(self, size=0):
+        """
+        Initialize the data
+        And validates if the number is an int
+        and > 0
+        Size: The size of the square
+        """
         self.__size = size
+
 
     # Property
     @property
     def size(self):
+        """ Gets the value """
         return self.__size
 
     # Setter modifies
