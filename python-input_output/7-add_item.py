3f281fec101588283b96b8a2fa5eb4e7d5b73ea8
davidperlaza14 <davidperlaza14@gmail.com>
2022-10-19 13:10:59 -0500

    7. Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """add_item
 """
+
 import sys
+"""import load and save"""
+
+
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
