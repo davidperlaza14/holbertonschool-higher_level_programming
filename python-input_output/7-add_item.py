1e06ae2101850177af4f79c28fa101571b8c905f
davidperlaza14 <davidperlaza14@gmail.com>
2022-10-19 13:23:16 -0500

    7. Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ import sys
 """import load and save"""
 
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 """Write a script that adds all arguments to a Python list, and then save them to a file:
 
