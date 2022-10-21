38e9db84cd869caba7a4338a3977b35b94afc2f6
davidperlaza14 <davidperlaza14@gmail.com>
2022-10-21 10:28:00 -0500

    models/

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -11,7 +11,7 @@ class Base:
     def __init__(self, id=None):
         if id is not None:
             self.id == int
-    
+        
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
