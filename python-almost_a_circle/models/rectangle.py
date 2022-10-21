78f3a2664a4e3067a2519a6b570fe06a64347ada
davidperlaza14 <davidperlaza14@gmail.com>
2022-10-21 10:50:13 -0500

    models/

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""rectangle
+"""
 from base import Base
 
 class Rectangle(Base):
+    """Inherits from Base
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
