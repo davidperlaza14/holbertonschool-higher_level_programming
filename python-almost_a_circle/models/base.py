bca3b35f022252472a0687153d3c051e2127bc13
davidperlaza14 <davidperlaza14@gmail.com>
2022-10-22 07:08:20 -0500

    models/

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 """
 
 
+import json
+
+
 class Base:
     """Base of the other shapes
     """
@@ -14,3 +17,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns JSON string representation
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
