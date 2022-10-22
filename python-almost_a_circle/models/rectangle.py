04b176378bb8f03c3c503842dcd0c1420ed1c612
davidperlaza14 <davidperlaza14@gmail.com>
2022-10-22 06:54:17 -0500

    models/

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -136,3 +136,11 @@ class Rectangle(Base):
                 pass
         else:
             print()
+    
+    def to_dictionary(self):
+        """Returns dict representation
+        """
+
+        return {'x': self.__x, 'y': self.__y, 'id': self.id,
+                'height': self.__height, 'width': self.__width}
+
