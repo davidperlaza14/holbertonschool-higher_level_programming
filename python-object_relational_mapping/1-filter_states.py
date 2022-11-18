ab3113a90547501cb11229fe6fce4e162b70914c
davidperlaza14 <davidperlaza14@gmail.com>
2022-11-17 19:42:44 -0500

    1. Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,14 +18,14 @@ def get_states_n():
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
-    
+
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     rows = cur.fetchall()
     for i in rows:
         print(i)
-    
+
     cur.close()
     db.close()
 
