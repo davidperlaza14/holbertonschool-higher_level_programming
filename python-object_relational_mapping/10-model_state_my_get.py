65c1a77f83004cfd0df8d42fe159a981f75c9bc5
davidperlaza14 <davidperlaza14@gmail.com>
2022-11-17 20:31:34 -0500

    10-model_state_my_get.py

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Conect the database"""
+import sys
+import MySQLdb
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+from model_state import Base
+from model_state import State
+
+
+def mysqlconnect():
+    engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:\
+        {sys.argv[2]}@localhost/{sys.argv[3]}")
+    with Session(bind=engine) as session:
+        x = session.query(State).filter(State.name == sys.argv[4]).first()
+        if x is None:
+            print("Not found")
+
+        print(x.id)
+
+
+if __name__ == '__main__':
+    mysqlconnect()
