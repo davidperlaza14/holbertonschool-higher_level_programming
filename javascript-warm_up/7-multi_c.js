8a111f7c5d830161755dc3fbf0fe89298dcc59a2
davidperlaza14 <davidperlaza14@gmail.com>
2022-12-21 12:23:06 -0500

    7. I love C

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myVar = process.argv[2];
+if (isNaN(myVar) === true) {
+    console.log('Missing number of occurrences');
+} else {
+    for (let x = 0; x < myVar; x++) {
+        console.log('C is fun');
+    }
+}
