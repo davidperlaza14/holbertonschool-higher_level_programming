c1df80b64ec1dd2dff7eb4ce90db605a6037e630
davidperlaza14 <davidperlaza14@gmail.com>
2022-12-21 12:43:36 -0500

    11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+function nextBiggest (arr) {
+    let max = 0; let result = 0;
+  
+    for (const value of arr) {
+      const i = Number(value);
+  
+      if (i > max) {
+        [result, max] = [max, i];
+      } else if (i < max && i > result) {
+        result = i;
+      }
+    }
+  
+    return result;
+  }
+  
+  console.log(nextBiggest(process.argv));
