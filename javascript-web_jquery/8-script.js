6531674956bf0a434894dfd8f0057dbc2da97c83
davidperlaza14 <davidperlaza14@gmail.com>
2022-12-23 15:31:00 -0500

    JQuery

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+$.ajax({
+    type: 'GET',
+    url: 'https://swapi-api.hbtn.io/api/films/?format=json',
+    success: function (data) {
+      $.each(data['results'], function (k, v) {
+        let title = v['title'];
+        $('UL#list_movies').append(`<li>${title}</li>`);
+      });
+    },
+    dataType: 'json'
+});
